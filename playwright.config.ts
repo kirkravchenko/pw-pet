68ea501fd1b9cc9b8a123eed860388fed7362ff7
kirkravchenko <kyrylkravchenko1994@gmail.com>
2024-12-24 13:06:34 +0100

    playwright report service

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'allure-playwright',
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    ['html', { open: 'never' }] // Disable serving the HTML report
+  ],
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
