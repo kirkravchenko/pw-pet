e74a42fff170abe43442443f0e1f4a52dfe188dd
kirkravchenko <kyrylkravchenko1994@gmail.com>
2024-12-24 12:42:29 +0100

    disabling automatic HTML report opening

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'allure-playwright',
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never' }] // Disable serving the HTML report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
