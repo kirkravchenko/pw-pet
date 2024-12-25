48044ae034d6f3d7cf9acab7591a7dd33203c8fa
kirkravchenko <kyrylkravchenko1994@gmail.com>
2024-12-25 11:23:56 +0100

    added eh test + screenshots

---
+++ tests/eh.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+
+test('has logo', async ({ page }) => {
+    await page.goto('https://www.everydayhealth.com/');
+
+    const logo = await page.locator('.header__logo a').screenshot();
+
+    test.info().attach('screenshot', {
+        body: logo,
+        contentType: 'image/png',
+    });
+
+    await expect(page).toHaveScreenshot({ 
+        maxDiffPixelRatio: 0.01,
+        stylePath: path.join(__dirname, 'screenshot.css')
+    });
+});
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,27 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//   await expect(page).toHaveTitle(/Playwright/);
 
-  // attach text
-  test.info().attach('failure reason', {
-    body: 'The title of the page is incorrect',
-    contentType: 'text/plain',
-  });
+//   // attach text
+//   test.info().attach('failure reason', {
+//     body: 'The title of the page is incorrect',
+//     contentType: 'text/plain',
+//   });
 
-  // attach screenshot
-  const screenshot = await page.getByRole('link', { 
-    name: 'Playwright logo Playwright' 
-  }).screenshot();
+//   // attach screenshot
+//   const screenshot = await page.getByRole('link', { 
+//     name: 'Playwright logo Playwright' 
+//   }).screenshot();
   
-  test.info().attach('screenshot', {
-    body: screenshot,
-    contentType: 'image/png',
-  });
+//   test.info().attach('screenshot', {
+//     body: screenshot,
+//     contentType: 'image/png',
+//   });
   
-  await expect(page).toHaveScreenshot();
-});
+//   await expect(page).toHaveScreenshot();
+// });
 
 // test('get started link', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');
